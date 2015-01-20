95f8d3e3c386be5470e7104074c5eeb1d4223fc0
Nick Hynes <nhynes@mit.edu>
2015-01-20 13:53:40 -0500

    Added htmlcss exercise

---
+++ src/exercises/htmlcss/checker.js
@@ -0,0 +1,2 @@
+// nice try ;)
+var _0x7a09=["\x68\x74\x6D\x6C","\x74\x69\x74\x6C\x65","\x59\x6F\x75\x72\x20\x74\x6F\x6B\x65\x6E\x20\x69\x73\x3A\x20","","\x6C\x6F\x67"];function checkExercise1(){var _0x51a5x2=$(_0x7a09[1])[_0x7a09[0]]();console[_0x7a09[4]](_0x7a09[2]+btoa(_0x51a5x2+atob(_0x7a09[3])));} ;
